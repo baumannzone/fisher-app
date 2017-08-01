ff48d7ccea866c3cd29d2219160a51528e9e2f4b
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-08-01 19:07:57 +0200

    Install firebase

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="dashboard">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard',
+  data() {
+    return {
+      msg: '#Dashboard',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import firebase from 'firebase';
+
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+const fBconfig = {
+  apiKey: 'AIzaSyAFIXqGSv2sqhKDJDAm-h4ZWUIUfijWSVs',
+  authDomain: 'fisher-app-db138.firebaseapp.com',
+  databaseURL: 'https://fisher-app-db138.firebaseio.com',
+  projectId: 'fisher-app-db138',
+  storageBucket: 'fisher-app-db138.appspot.com',
+  messagingSenderId: '869956372168',
+};
+firebase.initializeApp(fBconfig);
 
 /* eslint-disable no-new */
 new Vue({
