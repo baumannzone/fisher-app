884d943f9481bcfa31bc7a92cfe4c78ae015e34a
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-08-04 12:11:54 +0200

    Add dark sky weather API config

--- src/components/Crear.vue
+++ src/components/Crear.vue
@@ -1,6 +1,10 @@
 <template>
 
   <div class="crear">
+
+    <span>location:</span><input type="text" id="txtautocomplete" style="width:300px" placeholder="enter the adress"/>
+    <label id="lblresult"></label>
+
     <el-row>
       <el-col :xs="24" :sm="24" :md="{span: 20, offset:2}" :lg="{span: 16, offset:4}" :gutter="20">
         <h1>{{ msg }}</h1>
@@ -10,7 +14,8 @@
           <el-form-item label="Fecha de salida" required>
             <el-col :xs="24" :sm="11">
               <el-form-item prop="date">
-                <el-date-picker class="date" type="date" placeholder="Fecha" v-model="ruleForm.date"></el-date-picker>
+                <el-date-picker class="full-width" type="date" placeholder="Fecha"
+                                v-model="ruleForm.date"></el-date-picker>
               </el-form-item>
             </el-col>
           </el-form-item>
@@ -18,20 +23,37 @@
           <el-form-item label="Hora" required>
             <el-col :xs="24" :sm="11">
               <el-form-item prop="time1">
-                <el-time-select class="date" placeholder="Hora salida" :picker-options="pickerOptions" v-model="ruleForm.time1"></el-time-select>
+                <el-time-select class="full-width" placeholder="Hora salida" v-model="ruleForm.time1"
+                                :picker-options="timeOptions1"></el-time-select>
               </el-form-item>
             </el-col>
+
             <el-col :span="2" class="line">.</el-col>
+
             <el-col :xs="24" :sm="11">
               <el-form-item prop="time2">
-                <el-time-select class="date" placeholder="Hora llegada" :picker-options="pickerOptions" v-model="ruleForm.time2"></el-time-select>
+                <el-time-select class="full-width" placeholder="Hora llegada" v-model="ruleForm.time2"
+                                :picker-options="{ start: '00:00', step: '00:30', end: '23:59', minTime: ruleForm.time1 }">
+                </el-time-select>
               </el-form-item>
             </el-col>
+
           </el-form-item>
 
           <el-form-item label="Total Capturas" prop="totalCapturas" required>
             <el-col :xs="24" :sm="11">
-              <el-input-number v-model="ruleForm.totalCapturas" :min="0" :max="999" style="width: 100%;"></el-input-number>
+              <el-input-number class="full-width" v-model="ruleForm.totalCapturas" :min="0"
+                               :max="999"></el-input-number>
+            </el-col>
+          </el-form-item>
+
+          <el-form-item label="A bordo" prop="abordo" required>
+            <el-col :xs="24" :sm="11">
+              <el-select class="full-width" v-model="ruleForm.abordo" allow-create filterable multiple
+                         placeholder="Selecciona personal">
+                <el-option v-for="user in users" :key="user.value" :label="user.label" :value="user.value"></el-option>
+              </el-select>
+              </el-select>
             </el-col>
           </el-form-item>
 
@@ -39,37 +61,23 @@
             <el-input v-model="ruleForm.zonaPesca" placeholder="Nombre de roca, coordenadas, ..."></el-input>
           </el-form-item>
 
-          <el-form-item label="Activity zone" prop="region">
-            <el-select v-model="ruleForm.region" placeholder="Activity zone">
-              <el-option label="Zone one" value="shanghai"></el-option>
-              <el-option label="Zone two" value="beijing"></el-option>
-            </el-select>
+          <el-form-item label="Videos" prop="videos">
+            <el-input v-model="ruleForm.videos" placeholder="Youtube link"></el-input>
           </el-form-item>
 
-          <el-form-item label="Instant delivery" prop="delivery">
-            <el-switch on-text="" off-text="" v-model="ruleForm.delivery"></el-switch>
+          <el-form-item label="Fotos" prop="fotos">
+            <el-input v-model="ruleForm.fotos" placeholder="Fotos"></el-input>
           </el-form-item>
-          <el-form-item label="Activity type" prop="type">
-            <el-checkbox-group v-model="ruleForm.type">
-              <el-checkbox label="Online activities" name="type"></el-checkbox>
-              <el-checkbox label="Promotion activities" name="type"></el-checkbox>
-              <el-checkbox label="Offline activities" name="type"></el-checkbox>
-              <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
-            </el-checkbox-group>
+
+          <el-form-item label="Comentarios" prop="comentarios">
+            <el-input type="textarea" v-model="ruleForm.comentarios"></el-input>
           </el-form-item>
-          <el-form-item label="Resources" prop="resource">
-            <el-radio-group v-model="ruleForm.resource">
-              <el-radio label="Sponsorship"></el-radio>
-              <el-radio label="Venue"></el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="Activity form" prop="desc">
-            <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-          </el-form-item>
+
           <el-form-item>
             <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
             <el-button @click="resetForm('ruleForm')">Reset</el-button>
           </el-form-item>
+
         </el-form>
       </el-col>
     </el-row>
@@ -80,53 +88,73 @@
 </template>
 
 <script>
+  /* global google:true */
+  import darkSky from '../darksky/main';
+
+  console.debug( darkSky );
+
+  function intialize() {
+    const autocomplete = new google.maps.places.Autocomplete( document.getElementById( 'txtautocomplete' ) );
+    google.maps.event.addListener( autocomplete, 'place_changed', () => {
+      const place = autocomplete.getPlace();
+      let location = `Address: ${place.formatted_address} <br/>`;
+      location += `Latitude: ${place.geometry.location.lat()} <br/>`;
+      location += `Longitude: ${place.geometry.location.lng()}`;
+      document.getElementById( 'lblresult' ).innerHTML = location;
+    } );
+  }
+
+  google.maps.event.addDomListener( window, 'load', intialize );
+
 
   export default {
     name: 'crear',
     data() {
       return {
-        visible: false,
         msg: 'Crear',
-        pickerOptions: {
-          start: '00:00',
-          step: '00:30',
-          end: '23:59',
-        },
         ruleForm: {
           date: new Date(),
           time1: '',
           time2: '',
-          totalCapturas: '',
-          name: '',
-          region: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: '',
+          totalCapturas: 0,
+          zonaPesca: '',
+          abordo: [],
+          videos: '',
+          fotos: '',
+          comentarios: '',
         },
         rules: {
           date: [
             { type: 'date', required: true, message: 'Please pick a date', trigger: 'change' },
           ],
           time1: [
-            { type: 'date', required: true, message: 'Please pick a time', trigger: 'change' },
+            { required: true, message: 'Please pick a time', trigger: 'change' },
           ],
-          name: [
-            { required: true, message: 'Please input Activity name', trigger: 'blur' },
-            { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+          totalCapturas: [
+            { type: 'integer', required: true, message: 'Please set total', trigger: 'change' },
           ],
-          region: [
-            { required: true, message: 'Please select Activity zone', trigger: 'change' },
+          abordo: [
+            { type: 'array', required: true, message: 'Please select Activity zone', trigger: 'change' },
           ],
-          type: [
-            { type: 'array', required: true, message: 'Please select at least one activity type', trigger: 'change' },
-          ],
-          resource: [
-            { required: true, message: 'Please select activity resource', trigger: 'change' },
-          ],
-          desc: [
-            { required: true, message: 'Please input activity form', trigger: 'blur' },
-          ],
+        },
+        users: [
+          {
+            value: 'Jorge Baumann',
+            label: 'Jorge Baumann',
+          },
+          {
+            value: 'Jorge F. Baumann',
+            label: 'Jorge F. Baumann',
+          },
+          {
+            value: 'Angel Aguilar',
+            label: 'Ángel Aguilar',
+          },
+        ],
+        timeOptions1: {
+          start: '00:00',
+          step: '00:30',
+          end: '23:59',
         },
       };
     },
@@ -157,7 +185,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .date {
+  .full-width {
     width: 100%;
   }
 
---
+++ src/darksky/main.js
@@ -0,0 +1,9 @@
+// wheather API
+const darkSky = {
+  key: '9277ff4e8c9629652ddb0280ffa5d1f9',
+  example: 'https://api.darksky.net/forecast/9277ff4e8c9629652ddb0280ffa5d1f9/37.8267,-122.4233?exclude&lang=es',
+  api: 'https://api.darksky.net/forecast/',
+  options: '?exclude&lang=es',
+};
+
+export default darkSky;
