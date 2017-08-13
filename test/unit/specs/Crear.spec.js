96efe70ee92e7e7dbf3cb3876eb6505805132d7a
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-08-13 23:29:47 +0200

    Fix test

--- test/unit/specs/Crear.spec.js
+++ test/unit/specs/Crear.spec.js
@@ -5,7 +5,7 @@ describe( 'Crear.vue', () => {
   it( 'Should render main title correctly ', () => {
     const Constructor = Vue.extend( Crear );
     const vm = new Constructor().$mount();
-    expect( vm.$el.querySelector( '.h1' ).textContent )
+    expect( vm.$el.querySelector( 'h1' ).textContent )
       .to.equal( 'Crear' );
   } );
 } );
